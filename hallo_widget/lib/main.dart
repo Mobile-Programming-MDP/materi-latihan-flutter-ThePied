29aee2485e6a24b886b4c630ac561d8cdaea035c
Samuel Effendi Pratama 2226250024 <samuel291004@gmail.com>
2023-10-13 18:11:16 +0700

    Hello World

---
+++ hallo_widget/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Hallo Widget",
+        theme: ThemeData(primarySwatch: Colors.cyan),
+        home: Scaffold(
+          appBar: AppBar(title: const Text("Samuel Effendi")),
+          body: const Center(
+            child: Text("Hallo Samuel"),
+          ),
+        ));
+  }
+}
