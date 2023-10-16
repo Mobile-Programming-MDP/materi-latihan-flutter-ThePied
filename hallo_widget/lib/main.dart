772706a90b3235b8d38164d8ea7227df2045a38c
Samuel Effendi Pratama 2226250024 <samuel291004@gmail.com>
2023-10-16 18:06:22 +0700

    Commit 4

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,40 +14,122 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: "Hallo Widget",
         theme: ThemeData(primarySwatch: Colors.red),
-        home: FirstScreen()
-        );
+        home: const WidgetDemo());
   }
 }
 
-class FirstScreen extends StatelessWidget{
+class WidgetDemo extends StatelessWidget {
+  const WidgetDemo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget Demo'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                'Ini adalah contoh Penggunaan container',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Tombol Elevated'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              Text('Rating: 4.5'),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.network(
+              'https://picsum.photos/id/7/300/200',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
   const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         title: const Text("Samuel Effendi"),
         actions: <Widget>[
           IconButton(
-            onPressed:(){}, 
-            icon: const Icon(
-              Icons.search, 
-              color: Colors.white,
-          ))
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ))
         ],
         leading: IconButton(
           //icon di kiri appbar
-          onPressed: (){},
+          onPressed: () {},
           icon: const Icon(
             Icons.home,
             color: Colors.white,
           ),
         ),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Image(
+              image: NetworkImage(
+                'https://picsum.photos/id/88/300/200',
+              ),
+              width: 500, // Sesuaikan ukuran gambar sesuai kebutuhan
+              height: 500,
+            ),
+            Text(
+              "Jalan Di Barcelona",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+              Icons.location_on_rounded,
+              size: 50, // Sesuaikan ukuran ikon sesuai kebutuhan
+              color: Colors.red, // Sesuaikan warna ikon sesuai kebutuhan
+            )
+          ],
         ),
-          body: const Center(
-            child: Text("Hallo Samuel"),
-          ),
-        );
+      ),
+    );
   }
-
 }
