36025b637298e7717b8f0b904a04b84e6bf91f7a
Samuel Effendi Pratama 2226250024 <samuel291004@gmail.com>
2023-11-20 18:09:42 +0700

    Pertemuan

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:wisata_candi/screens/home_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/search_screen.dart';
 import 'package:wisata_candi/screens/sign_in_screen.dart';
+import 'package:wisata_candi/screens/sign_up_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: MainScreen(),
+      home: SignUpScreen(),
       // home: HomeScreen(),
       // home: SearchScreen(),
       // home: const ProfileScreen(),
